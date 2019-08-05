4ae7461fdcf1606c72f193f0fa3a033f8e1f93ef
Tshepiso Mogapi <tshepisomogapi@setups-MBP.ace.local>
2019-08-05 18:34:18 +0200

    add edit texts and save menu

--- app/src/main/java/com/example/travelmantics/AdminActivity.java
+++ app/src/main/java/com/example/travelmantics/AdminActivity.java
@@ -2,6 +2,8 @@ package com.example.travelmantics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,14 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.save_menu, menu);
+
+        return true;
     }
 }
