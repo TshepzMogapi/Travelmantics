682ec042975d854a383a456e430f4a4fb56ce9a6
Tshepiso Mogapi <tshepisomogapi@Tshepisos-MacBook-Pro.local>
2019-08-05 23:42:06 +0200

    add menu to navigate to add Deal activity

--- app/src/main/java/com/example/travelmantics/DealActivity.java
+++ app/src/main/java/com/example/travelmantics/DealActivity.java
@@ -14,7 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class AdminActivity extends AppCompatActivity {
+public class DealActivity extends AppCompatActivity {
 
     private FirebaseDatabase mFirebaseDatabase;
 
